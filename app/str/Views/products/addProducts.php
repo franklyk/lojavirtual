4ae3f43d0af0890e2bc6cf2993017c605927f930
Franklin da Silva Batista <frsbatist@gmail.com>
2024-03-02 20:12:31 -0300

    Refatorar
    Reestruturação para a diminuição da quantidade de bibliotecas.
    Otimização do código.
    Iniciando pela página home.

---
+++ app/str/Views/products/addProducts.php
@@ -0,0 +1,113 @@
+<!--================Login Box Area =================-->
+<section class="login_box_area section_gap">
+    <div class="container">
+        <div class="row">
+
+            <!-- <div class="col-lg-12"> -->
+                <!-- <div class="login_form_inner"> -->
+                    <form class="row login_form container-form" action="" method="post" id="contactForm">
+                        <h3>Log in to enter</h3>
+                        <div class="row-input">
+                            <div class="col-md-12 form-group">
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Username" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Username'">
+                            </div>
+                            <div class="col-md-12 form-group">
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
+                            </div>
+                        </div>
+
+                        <div class="col-md-12 form-group">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
+                        </div>
+                        <div class="col-md-12 form-group">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
+                        </div>
+                        <div class="col-md-12 form-group">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'">
+                        </div>
+
+                        <div class="col-md-12 form-group">
+                            <button type="submit" value="submit" class="primary-btn">Log In</button>
+                        </div>
+                    </form>
+                <!-- </div> -->
+            <!-- </div> -->
+        </div>
+    </div>
+</section>
+<div class="wrapper">
+            <div class="row">
+                <div class="top-list">
+                    <span class="title-content">Formulário</span>
+                    <div class="top-list-right">
+                        <a href="listar.html" class="btn-info">Listar</a>
+                    </div>
+                </div>
+
+                <div class="content-adm">
+                    <form class="form-adm">
+                        <div class="row-input">
+                            <div class="column">
+                                <label class="title-input">Nome</label>
+                                <input type="text" name="name" id="name" class="input-adm" placeholder="Nome completo">
+                            </div>
+                            <div class="column">
+                                <label class="title-input">E-mail</label>
+                                <input type="email" name="email" id="email" class="input-adm"
+                                    placeholder="Melhor e-mail">
+                            </div>
+                        </div>
+
+                        <div class="row-input">
+                            <div class="column">
+                                <label class="title-input">Campo 3</label>
+                                <input type="text" name="campo_3" id="campo_3" class="input-adm" placeholder="Campo 3">
+                            </div>
+                            <div class="column">
+                                <label class="title-input">Campo 4</label>
+                                <input type="text" name="campo_4" id="campo_4" class="input-adm" placeholder="Campo 4">
+                            </div>
+                        </div>
+
+                        <div class="row-input">
+                            <div class="column">
+                                <label class="title-input">Campo 5</label>
+                                <input type="text" name="campo_5" id="campo_5" class="input-adm" placeholder="Campo 5">
+                            </div>
+                        </div>
+
+                        <div class="row-input">
+                            <div class="column">
+                                <label class="title-input">Campo 6</label>
+                                <select name="campo_6" id="campo_6" class="input-adm">
+                                    <option>Selecione</option>
+                                    <option>Opção 1</option>
+                                    <option>Opção 2</option>
+                                    <option>Opção 3</option>
+                                </select>
+                            </div>
+                            <div class="column">
+                                <label class="title-input">Campo 7</label>
+                                <select name="campo_7" id="campo_7" class="input-adm">
+                                    <option>Selecione</option>
+                                    <option>Opção 1</option>
+                                    <option>Opção 2</option>
+                                    <option>Opção 3</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="row-input">
+                            <div class="column">
+                                <label class="title-input">Descrição</label>
+                                <textarea name="descricao" id="descricao" class="input-adm"
+                                    placeholder="Descrição"></textarea>
+                            </div>
+                        </div>
+
+                        <button type="button" class="btn-success">Cadastrar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+<!--================End Login Box Area =================-->
