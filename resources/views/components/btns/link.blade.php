3810df90fdee3884a3944e01fbaf04c7089c4ab3
Franklin da Silva Batista <frsbatist@gmail.com>
2024-07-12 14:35:47 -0300

    Css
    Personalização de cores.

--- resources/views/components/btns/link.blade.php
+++ resources/views/components/btns/link.blade.php
@@ -2,7 +2,7 @@
 
 @if ($type == 'a')
     <li class="item-menu">
-        <a href="{{ $link }}" class="{{ $active ? 'bg-blue text-white' : 'bg-white text-info' }} link-menu"
+        <a href="{{ $link }}" class="{{ $active ? 'bg-site-color-light text-white' : 'bg-site-color text-white' }} link-menu"
             aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }} >
             <div class="icon"><i class="{{ $icon }}"></i></div>
             <span class="label-link">{{ $label }}</span>
@@ -10,7 +10,7 @@
     </li>
 @else
     <li class="item-menu">
-        <button class="bg-white text-info link-menu" >
+        <button class="bg-site-color text-white link-menu" >
             <div class="icon"><i class="{{ $icon }}"></i></div>
             <span class="label-link">{{ $label }}</span>
         </button>
